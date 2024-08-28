60d0c43812a5c3a39ef9b842c9ca13dbfa556505
dazzlec123 <dazzlec123@gmail.com>
2024-08-28 19:41:47 +0000

    Aug 28, 2024, 3:41 PM

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,6 @@
 // Using this file to track local file location constants
 
 pub const ASSET_REGISTRY_FOLDER: &str = "C:/Users/dazzl/CodingProjects/substrate/polkadot_assets/assets/asset_registry/";
+pub const ASSET_IGNORE_LIST: &str = "C:/Users/dazzl/CodingProjects/substrate/polkadot_assets/assets/ignore_list.json";
 pub const LP_REGISTRY_FOLDER: &str = "C:/Users/dazzl/CodingProjects/substrate/polkadot_assets/lps/lp_registry/";
 pub const XCM_FEE_BOOK: &str = "C:/Users/dazzl/CodingProjects/substrate/xcm-test/data/newEventFeeBook.json";
