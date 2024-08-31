45dcc98462dbf13e8cc93317f89298f8d47f5dff
dazzlec123 <dazzlec123@gmail.com>
2024-08-31 18:24:34 +0000

    Aug 31, 2024, 2:24 PM

--- src/lib.rs
+++ src/lib.rs
@@ -320,7 +320,11 @@ pub async fn async_search_best_path_a_to_b(start_key: String, destination_key: S
 
 }
 
-
+///  *Works
+/// 
+/// Called by 'search_polkadot'. Search 2000 DOT -> 2000 DOT
+/// 
+/// 
 pub async fn async_search_default_polkadot(){
     let start_key = "2000{\"NativeAssetId\":{\"Token\":\"DOT\"}}".to_string();
     let destination_key = "2000{\"NativeAssetId\":{\"Token\":\"DOT\"}}".to_string();
